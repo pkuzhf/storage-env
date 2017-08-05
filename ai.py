794424de34364d39733db9718c9275bd1b3b2784
gzlong96 <30570937+gzlong96@users.noreply.github.com>
2017-08-05 17:32:03 +0800

    Update ai.py

--- ai.py
+++ ai.py
@@ -5,6 +5,7 @@ import config, utils
 import matplotTest as draw
 import mapGenerator as MG
 import copy
+import os
 
 
 # source_pos = [[0,0]]
@@ -75,7 +76,7 @@ def AStar(agent_id, start_pos, end_pos, agent_pos, reserve, t0):
         open_set.pop(idx)
         close_set.append([pos, t])
 
-        #dirs = utils.dirs + [[0, 0]]
+        # dirs = utils.dirs + [[0, 0]]
         dirs = utils.dirs
         for i in range(len(dirs)):
             a = dirs[i]
@@ -155,102 +156,114 @@ def addReserve(entry, agent_id, reserve, schedule):
             removeSchedule(i, reserve, schedule)
     reserve[entry] = agent_id
 
+if not os.path.exists('result'):
+    os.mkdir('result')
 
 for agent_num in range(1, 50):
+    for round in range(5):
 
-    env = AGENT_GYM(source_pos, hole_pos, agent_num, total_time, hole_city, city_dis)
-    env.seed(config.Game.Seed)
+        # save static info
+        log = open('result/mapInfo'+str(agent_num)+'_'+str(round)+'.log', 'w')
+        log.write(str([config.Map.Width,config.Map.Height])+'\n')
+        log.write(str(city_dis)+'\n')
+        log.write(str(source_pos) + '\n')
+        log.write(str(hole_pos) + '\n')
+        log.write(str(hole_city) + '\n')
 
-    #print 'agent_num: ' + str(agent_num)
-    observation = env.reset()
-    #print observation
-    [agent_pos, agent_city, agent_reward, hole_reward, source_reward] = observation
 
-    agent_city_old = copy.deepcopy(agent_city)
-    agent_pos_old = copy.deepcopy(agent_pos)
+        env = AGENT_GYM(source_pos, hole_pos, agent_num, total_time, hole_city, city_dis)
+        env.seed(config.Game.Seed)
 
-    reserve = {}
-    schedule = [[]] * agent_num
-    for time in range(total_time):
-        #print ['*** time ***', time]
-        env.render()
+        #print 'agent_num: ' + str(agent_num)
+        observation = env.reset()
+        #print observation
+        [agent_pos, agent_city, agent_reward, hole_reward, source_reward] = observation
 
-        # random action
-        # action = []
-        # for i in range(agent_num):
-        #     r = np.random.random_integers(0, 4)
-        #     if r < 4:
-        #         action.append(utils.dirs[r])
-        #     else:
-        #         action.append([0,0])
+        agent_city_old = copy.deepcopy(agent_city)
+        agent_pos_old = copy.deepcopy(agent_pos)
 
-        # pathfinding
+        reserve = {}
+        schedule = [[]] * agent_num
+        for time in range(total_time):
+            #print ['*** time ***', time]
+            env.render()
 
-        action = []
-        for i in range(agent_num):
-            #print 'agent ' + str(i) + ' in pos ' + str(agent_pos[i])
-            if len(schedule[i]) == 0:
-                schedule[i] = pathfinding(reserve, agent_pos, agent_city, i, time)
-                for [pos, t, a] in schedule[i]:
-                    addReserve(encode(pos, t), i, reserve, schedule)
-                    addReserve(encode(pos, t + 1), i, reserve, schedule)
-            elif len(schedule[i]) == 1:
-                removeSchedule(i, reserve, schedule)
-                schedule[i] = pathfinding(reserve, agent_pos, agent_city, i, time)
-                for [pos, t, a] in schedule[i]:
-                    addReserve(encode(pos, t), i, reserve, schedule)
-                    addReserve(encode(pos, t + 1), i, reserve, schedule)
-            else:
-                [pos, t, a] = schedule[i][0]
-                if pos != agent_pos[i]:
+            # random action
+            # action = []
+            # for i in range(agent_num):
+            #     r = np.random.random_integers(0, 4)
+            #     if r < 4:
+            #         action.append(utils.dirs[r])
+            #     else:
+            #         action.append([0,0])
+
+            # pathfinding
+
+            action = []
+            for i in range(agent_num):
+                #print 'agent ' + str(i) + ' in pos ' + str(agent_pos[i])
+                if len(schedule[i]) == 0:
+                    schedule[i] = pathfinding(reserve, agent_pos, agent_city, i, time)
+                    for [pos, t, a] in schedule[i]:
+                        addReserve(encode(pos, t), i, reserve, schedule)
+                        addReserve(encode(pos, t + 1), i, reserve, schedule)
+                elif len(schedule[i]) == 1:
                     removeSchedule(i, reserve, schedule)
                     schedule[i] = pathfinding(reserve, agent_pos, agent_city, i, time)
                     for [pos, t, a] in schedule[i]:
                         addReserve(encode(pos, t), i, reserve, schedule)
                         addReserve(encode(pos, t + 1), i, reserve, schedule)
-            #print ['schedule', schedule[i]]
-            [pos, t, a] = schedule[i][0]
-            del schedule[i][0]
-            del reserve[encode(pos, t)]
-            if a != [0, 0]:
-                del reserve[encode(pos, t + 1)]
-            action.append(a)
+                else:
+                    [pos, t, a] = schedule[i][0]
+                    if pos != agent_pos[i]:
+                        removeSchedule(i, reserve, schedule)
+                        schedule[i] = pathfinding(reserve, agent_pos, agent_city, i, time)
+                        for [pos, t, a] in schedule[i]:
+                            addReserve(encode(pos, t), i, reserve, schedule)
+                            addReserve(encode(pos, t + 1), i, reserve, schedule)
+                #print ['schedule', schedule[i]]
+                [pos, t, a] = schedule[i][0]
+                del schedule[i][0]
+                del reserve[encode(pos, t)]
+                if a != [0, 0]:
+                    del reserve[encode(pos, t + 1)]
+                action.append(a)
 
 
-        #print ['action', action]
-        observation, reward, done, info = env.step(action)
-        [agent_pos, agent_city, agent_reward, hole_reward, source_reward] = observation
-        #print ['agent_pos', agent_pos]
+            #print ['action', action]
+            observation, reward, done, info = env.step(action)
+            [agent_pos, agent_city, agent_reward, hole_reward, source_reward] = observation
+            #print ['agent_pos', agent_pos]
 
-        for i in range(agent_num):
-            addReserve(encode(agent_pos[i], time + 1), i, reserve, schedule)
-            addReserve(encode(agent_pos[i], time + 2), i, reserve, schedule)
+            for i in range(agent_num):
+                addReserve(encode(agent_pos[i], time + 1), i, reserve, schedule)
+                addReserve(encode(agent_pos[i], time + 2), i, reserve, schedule)
 
-        '''
-        if True:
-            # make pics
+            # save dynamic info
             for i in range(len(agent_pos)):
                 agent_pos_old[i][0] = (agent_pos[i][0] + agent_pos_old[i][0]) / 2.0
                 agent_pos_old[i][1] = (agent_pos[i][1] + agent_pos_old[i][1]) / 2.0
-
-                # so many params...
-            if not time == 0:
-                draw.draw_map([config.Map.Width, config.Map.Height], source_pos, hole_pos, hole_city, agent_pos_old,
-                              agent_city_old,color,"mapOutput", "show", 2 * time - 1, agent_reward, hole_reward, source_reward, city_dis)
-
-            draw.draw_map([config.Map.Width, config.Map.Height], source_pos, hole_pos, hole_city, agent_pos, agent_city,
-                          color, "mapOutput", "show", 2 * time, agent_reward, hole_reward, source_reward, city_dis)
-
+            if time!=0:
+                log.write(str(agent_pos_old)+'\n')
+                log.write(str(agent_city_old)+'\n')
+            log.write(str(agent_reward) + '\n')
+            log.write(str(source_reward) + '\n')
+            log.write(str(hole_reward) + '\n')
+            log.write(str(agent_pos) + '\n')
+            log.write(str(agent_city) + '\n')
             for i in range(len(agent_pos)):
                 agent_city_old[i] = agent_city[i]
                 agent_pos_old[i][0] = agent_pos[i][0]
                 agent_pos_old[i][1] = agent_pos[i][1]
-        '''
 
-        if done:
-            #print("Episode finished after {} timesteps".format(time+1))
-            break
-    print str(agent_num) + '\t' + str(sum(agent_reward))
-# draw.save_video("mapOutput", "show", total_time) # avi with cv
+            if done:
+                #print("Episode finished after {} timesteps".format(time+1))
+                break
+        print str(agent_num) + '\t' + str(sum(agent_reward))
+        log.write('end')
+        log.close()
+
+        # draw.draw_log('result/mapInfo'+str(agent_num)+'_'+str(round)+'.log', 'logtest', 'demo')
 # draw.save_video2("mapOutput", "show", total_time) # gif with imageio
-# draw.save_video3("mapOutput", "show", total_time) # mp4 with imageio
+#         draw.save_video3("mapOutput2_"+str(round), "show", total_time) # mp4 with imageio
+
